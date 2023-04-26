f0fc62df32a479ef83ce2d636c286b709735e85c
ArmandCT <acasadot@outlook.com>
2023-04-26 19:19:19 +0200

    dades

--- informador.py
+++ informador.py
@@ -2,6 +2,6 @@
 
 def informador(poblacio, pais, temperatura, humitat, presio, data, pluja, condicions, humitat_optima, cultiu):
     try:
-        print(f"{poblacio} ({pais}) a data i hora {data}: \n Condicions ambientals: {condicions} \n Temperatura: {temperatura} ºC \n Humitat relativa: {humitat} % \n Pressió atmosfèrica: {presio} mbars \n Precipitació: {pluja} mm \n L'humitat òptima del {cultiu} és del {humitat_optima} %")
+        print(f"{poblacio} ({pais}) a data i hora {data}: \n Condicions ambientals: {condicions} \n Temperatura: {temperatura} ºC \n Humitat relativa: {humitat} % \n Pressió atmosfèrica: {presio} mbars \n Precipitació: {pluja} mm/h \n L'humitat òptima del {cultiu} és del {humitat_optima} %")
     except Exception as e:
         print(f"Hi ha hagut un error amb la funció informador >>>>> {e}")
